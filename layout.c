11df7e1ca88f66adc48736431399d5b1bc8134d5
Yerlotic <Yerlotic@users.noreply.github.com>
2025-06-25 12:06:54 +0000

    Initial commit

---
+++ layout.c
@@ -0,0 +1,367 @@
+#include "clay.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "colors.h"
+
+const int FONT_ID_BODY_16 = 0;
+const int FONT_LOAD_SIZE = 40;
+const int button_font_size = 20;
+const int sidebar_font_size = 23;
+const int document_font_size = 30;
+const int title_font_size = 40;
+
+#define BUTTON_RADIUS CLAY_CORNER_RADIUS(10)
+#define LAYOUT_RADIUS CLAY_CORNER_RADIUS(10)
+
+void RenderHeaderButton(Clay_String text) {
+    CLAY({
+        .layout = {
+            .padding = { 16, 16, 8, 8 },
+            .childAlignment = {
+                .x = CLAY_ALIGN_X_CENTER,
+                .y = CLAY_ALIGN_Y_CENTER,
+            },
+            // .sizing = CLAY_SIZING_FIT(200, 200)
+        },
+        .backgroundColor = BUTTON_COLOR,
+        .id = CLAY_SID(text),
+        .cornerRadius = BUTTON_RADIUS,
+    }) {
+        CLAY_TEXT(text, CLAY_TEXT_CONFIG({
+            .fontId = FONT_ID_BODY_16,
+            .fontSize = button_font_size,
+            .textColor = COLOR_TEXT,
+            .textAlignment = CLAY_TEXT_ALIGN_CENTER,
+            .wrapMode = CLAY_TEXT_WRAP_NONE,
+        }));
+    }
+}
+
+void RenderDropdownMenuItem(Clay_String text) {
+    CLAY({
+        .backgroundColor = COLOR_SURFACE0,
+        .cornerRadius = BUTTON_RADIUS,
+        .id = CLAY_SID(text),
+        .layout = {
+            .padding = CLAY_PADDING_ALL(16),
+            .sizing = {
+                .width = CLAY_SIZING_GROW(0)
+            },
+    }}) {
+        CLAY_TEXT(text, CLAY_TEXT_CONFIG({
+            .fontId = FONT_ID_BODY_16,
+            .fontSize = button_font_size,
+            .textColor = COLOR_TEXT,
+        }));
+    }
+}
+
+typedef struct {
+    Clay_String title;
+    Clay_String contents;
+} Document;
+
+typedef struct {
+    Document *documents;
+    uint32_t length;
+} DocumentArray;
+
+Document documentsRaw[5];
+
+DocumentArray documents = {
+    .length = 3,
+    .documents = documentsRaw
+};
+
+typedef struct {
+    intptr_t offset;
+    intptr_t memory;
+} ClayVideoDemo_Arena;
+
+enum {
+    MAGICK_BEST_FIT       = 1 << 0,
+    MAGICK_TRANSPARENT_BG = 1 << 1,
+    MAGICK_OPEN_ON_DONE   = 1 << 2,
+} MagickState;
+
+typedef struct {
+    int32_t selectedDocumentIndex;
+    float yOffset;
+    ClayVideoDemo_Arena frameArena;
+    bool shouldClose;
+    int state;
+    char* resultFile;
+    uint16_t resize;  // max: 6 bytes for string repr
+    char resize_str[6]; // max: 6 bytes for string repr
+} ClayVideoDemo_Data;
+
+
+typedef struct {
+    uint32_t requestedDocumentIndex;
+    uint32_t* selectedDocumentIndex;
+    int* state;
+} SidebarClickData;
+
+void HandleSidebarInteraction(
+    Clay_ElementId elementId,
+    Clay_PointerData pointerData,
+    intptr_t userData
+) {
+    SidebarClickData *clickData = (SidebarClickData*)userData;
+    // If this button was clicked
+    if (pointerData.state == CLAY_POINTER_DATA_PRESSED_THIS_FRAME) {
+        if (clickData->requestedDocumentIndex >= 0 && clickData->requestedDocumentIndex < documents.length) {
+            if (clickData->requestedDocumentIndex == 0) {
+                *clickData->state = *clickData->state ^ MAGICK_BEST_FIT;
+            } else if (clickData->requestedDocumentIndex == 1) {
+                *clickData->state = (*clickData->state ^ MAGICK_TRANSPARENT_BG);
+            } else if (clickData->requestedDocumentIndex == 2) {
+                *clickData->state = (*clickData->state ^ MAGICK_OPEN_ON_DONE);
+            }
+            // Select the corresponding document
+            *clickData->selectedDocumentIndex = clickData->requestedDocumentIndex;
+        }
+    }
+}
+
+ClayVideoDemo_Data ClayVideoDemo_Initialize() {
+    documents.documents[0] = (Document){ .title = CLAY_STRING("Best fit"), .contents = CLAY_STRING("This ashlar option aligns images on both sides of the resulting image") };
+    documents.documents[1] = (Document){ .title = CLAY_STRING("Transparent background"), .contents = CLAY_STRING("Makes the background transparent") };
+    documents.documents[2] = (Document){ .title = CLAY_STRING("Open when done"), .contents = CLAY_STRING("Enable this to see the result right after it's done!\n\nNothing more\nsurely") };
+    // documents.documents[3] = (Document){ .title = CLAY_STRING("Article 4"), .contents = CLAY_STRING("Article 4") };
+    // documents.documents[4] = (Document){ .title = CLAY_STRING("Article 5"), .contents = CLAY_STRING("Article 5") };
+
+    ClayVideoDemo_Data data = {
+        .frameArena = { .memory = (intptr_t)malloc(1024) },
+        .shouldClose = false,
+        .state = MAGICK_BEST_FIT | MAGICK_TRANSPARENT_BG | MAGICK_OPEN_ON_DONE,
+        .selectedDocumentIndex = 0,
+        .resultFile = "",
+        .resize_str = "",
+        .resize = 1000,
+    };
+    return data;
+}
+
+Clay_RenderCommandArray ClayVideoDemo_CreateLayout(ClayVideoDemo_Data *data) {
+    data->frameArena.offset = 0;
+
+    Clay_BeginLayout();
+
+    Clay_Sizing layoutExpand = {
+        .width = CLAY_SIZING_GROW(0),
+        .height = CLAY_SIZING_GROW(0)
+    };
+
+    Clay_Color contentBackgroundColor = COLOR_BASE;
+
+    // Build UI here
+    CLAY({
+        .id = CLAY_ID("OuterContainer"),
+        // .backgroundColor = { 43, 41, 51, 255 },
+        .backgroundColor = COLOR_MANTLE,
+        .layout = {
+            .layoutDirection = CLAY_TOP_TO_BOTTOM,
+            .sizing = layoutExpand,
+            .padding = CLAY_PADDING_ALL(16),
+            .childGap = 16,
+        }
+    }) {
+        // Child elements go inside braces
+        CLAY({ .id = CLAY_ID("HeaderBar"),
+            .layout = {
+                .sizing = {
+                    .height = CLAY_SIZING_FIXED(60),
+                    .width = CLAY_SIZING_GROW(0)
+                },
+                .padding = { 16, 16, 16, 16 },
+                .childGap = 16,
+                .childAlignment = {
+                    .y = CLAY_ALIGN_Y_CENTER,
+                    .x = CLAY_ALIGN_X_CENTER,
+                }
+            },
+            .cornerRadius = LAYOUT_RADIUS,
+            .backgroundColor = contentBackgroundColor,
+        }) {
+            // Header buttons go here
+            CLAY({
+                .id = CLAY_ID("FileButton"),
+                .layout = { .padding = { 16, 16, 8, 8 }},
+                .backgroundColor = BUTTON_COLOR,
+                .cornerRadius = BUTTON_RADIUS,
+            }) {
+                CLAY_TEXT(CLAY_STRING("File"), CLAY_TEXT_CONFIG({
+                    .fontId = FONT_ID_BODY_16,
+                    .fontSize = button_font_size,
+                    .textColor = COLOR_TEXT,
+                }));
+
+                bool fileMenuVisible =
+                    Clay_PointerOver(Clay_GetElementId(CLAY_STRING("FileButton")))
+                    ||
+                    Clay_PointerOver(Clay_GetElementId(CLAY_STRING("FileMenu")));
+
+                if (fileMenuVisible) {
+                    CLAY({
+                        .id = CLAY_ID("FileMenu"),
+                        .floating = {
+                            .attachTo = CLAY_ATTACH_TO_PARENT,
+                            .attachPoints = {
+                                .parent = CLAY_ATTACH_POINT_LEFT_BOTTOM
+                            },
+                        },
+                        .layout = {
+                            .padding = { 0, 0, 8, 8 }
+                        }
+                    }) {
+                        CLAY({
+                            .layout = {
+                                .layoutDirection = CLAY_TOP_TO_BOTTOM,
+                                .sizing = {
+                                    .width = CLAY_SIZING_FIXED(200)
+                                },
+                            },
+                            .backgroundColor = COLOR_SURFACE0,
+                            .cornerRadius = BUTTON_RADIUS,
+                        }) {
+                            // Render dropdown items here
+                            RenderDropdownMenuItem(CLAY_STRING("Open result"));
+                            RenderDropdownMenuItem(CLAY_STRING("Quit"));
+                        }
+                    }
+                }
+            }
+            RenderHeaderButton(CLAY_STRING("Select_Images"));
+            CLAY({ .layout = { .sizing = { CLAY_SIZING_GROW(0) }}}) {}
+            RenderHeaderButton(CLAY_STRING(u8"Upload"));
+
+            CLAY({
+                .layout = { .padding = { 16, 16, 8, 8 }},
+                .backgroundColor = BUTTON_COLOR,
+                .id = CLAY_ID("Resize"),
+                .cornerRadius = BUTTON_RADIUS,
+            }) {
+                CLAY_TEXT(CLAY_STRING("Resize: "), CLAY_TEXT_CONFIG({
+                    .fontId = FONT_ID_BODY_16,
+                    .fontSize = button_font_size,
+                    .textColor = COLOR_TEXT,
+                }));
+                Clay_String clay_str = (Clay_String) {
+                    .length = strlen(data->resize_str),
+                    .chars = data->resize_str,
+                    .isStaticallyAllocated = true,
+                    // .isStaticallyAllocated = false,
+                };
+                CLAY_TEXT(clay_str, CLAY_TEXT_CONFIG({
+                    .fontId = FONT_ID_BODY_16,
+                    .fontSize = button_font_size,
+                    .textColor = COLOR_TEXT,
+                }));
+                // CLAY_TEXT(clay_str,
+                //         // (CLAY__INIT(Clay_String) { .isStaticallyAllocated = true, .length = strlen(str), .chars = (str) }),
+                //         CLAY_TEXT_CONFIG({
+                //             .fontId = FONT_ID_BODY_16,
+                //             .fontSize = button_font_size,
+                //             .textColor = COLOR_TEXT,
+                // }));
+                // CLAY({
+                //
+                // }) {
+                //
+                // }
+            }
+            RenderHeaderButton(CLAY_STRING("Media"));
+            RenderHeaderButton(CLAY_STRING("Support"));
+        }
+
+        CLAY({
+            .id = CLAY_ID("LowerContent"),
+            .layout = { .sizing = layoutExpand, .childGap = 16 },
+
+        }) {
+            CLAY({
+                .id = CLAY_ID("Sidebar"),
+                .backgroundColor = contentBackgroundColor,
+                .layout = {
+                    .layoutDirection = CLAY_TOP_TO_BOTTOM,
+                    .padding = CLAY_PADDING_ALL(16),
+                    .childGap = 8,
+                    .sizing = {
+                        .width = CLAY_SIZING_FIXED(250),
+                        .height = CLAY_SIZING_GROW(0)
+                    },
+                },
+                .cornerRadius = LAYOUT_RADIUS,
+            }) {
+                for (uint32_t i = 0; i < documents.length; i++) {
+                    Document document = documents.documents[i];
+                    Clay_LayoutConfig sidebarButtonLayout = {
+                        .sizing = { .width = CLAY_SIZING_GROW(0) },
+                        .padding = CLAY_PADDING_ALL(16)
+                    };
+
+                    SidebarClickData *clickData = (SidebarClickData *)(data->frameArena.memory + data->frameArena.offset);
+                    *clickData = (SidebarClickData) { .requestedDocumentIndex = i, .selectedDocumentIndex = &data->selectedDocumentIndex, .state = &data->state };
+                    data->frameArena.offset += sizeof(SidebarClickData);
+                    if (i == 0 && data->state & MAGICK_BEST_FIT ||
+                        i == 1 && data->state & MAGICK_TRANSPARENT_BG ||
+                        i == 2 && data->state & MAGICK_OPEN_ON_DONE) {
+                        CLAY({
+                            .layout = sidebarButtonLayout,
+                            .backgroundColor = OPACITY(COLOR_GREEN, 35),
+                            .cornerRadius = CLAY_CORNER_RADIUS(8)
+                        }) {
+                            Clay_OnHover(HandleSidebarInteraction, (intptr_t)clickData);
+                            CLAY_TEXT(document.title, CLAY_TEXT_CONFIG({
+                                .fontId = FONT_ID_BODY_16,
+                                .fontSize = sidebar_font_size,
+                                .textColor = COLOR_TEXT,
+                            }));
+                        }
+                    } else {
+                        CLAY({ .layout = sidebarButtonLayout, .backgroundColor = TOGGLE_COLOR, .cornerRadius = CLAY_CORNER_RADIUS(8) }) {
+                            Clay_OnHover(HandleSidebarInteraction, (intptr_t)clickData);
+                            CLAY_TEXT(document.title, CLAY_TEXT_CONFIG({
+                                        .fontId = FONT_ID_BODY_16,
+                                        .fontSize = sidebar_font_size,
+                                        .textColor = COLOR_TEXT,
+                                        }));
+                        }
+                    }
+                }
+            }
+
+            CLAY({ .id = CLAY_ID("MainContent"),
+                .backgroundColor = contentBackgroundColor,
+                .clip = { .vertical = true, .childOffset = Clay_GetScrollOffset() },
+                .layout = {
+                    .layoutDirection = CLAY_TOP_TO_BOTTOM,
+                    .childGap = 16,
+                    .padding = CLAY_PADDING_ALL(16),
+                    .sizing = layoutExpand
+                },
+                .cornerRadius = LAYOUT_RADIUS,
+            }) {
+                Document selectedDocument = documents.documents[data->selectedDocumentIndex];
+                CLAY_TEXT(selectedDocument.title, CLAY_TEXT_CONFIG({
+                    .fontId = FONT_ID_BODY_16,
+                    .fontSize = title_font_size,
+                    .textColor = COLOR_TEXT
+                }));
+                CLAY_TEXT(selectedDocument.contents, CLAY_TEXT_CONFIG({
+                    .fontId = FONT_ID_BODY_16,
+                    .fontSize = document_font_size,
+                    .textColor = COLOR_TEXT
+                }));
+            }
+        }
+    }
+
+    Clay_RenderCommandArray renderCommands = Clay_EndLayout();
+    for (int32_t i = 0; i < renderCommands.length; i++) {
+        Clay_RenderCommandArray_Get(&renderCommands, i)->boundingBox.y += data->yOffset;
+    }
+    return renderCommands;
+}
---
+++ main.c
@@ -0,0 +1,286 @@
+#include <stdio.h>
+#include <unistd.h>
+#define CLAY_IMPLEMENTATION
+#include "clay.h"
+#define NOB_IMPLEMENTATION
+#include "nob.h"
+#include "raylib/raylib.h"
+#include "raylib/clay_renderer_raylib.c"
+#include "layout.c"
+#include <linux/limits.h>
+#include <assert.h>
+#include <fontconfig/fontconfig.h>
+
+#define TESTING
+
+// ascii file separator
+// thx ascii!
+// #define MULTI_SEPARATOR '\x1c'
+#define MULTI_SEPARATOR '|'
+#include "tinyfiledialogs.c"
+
+typedef struct {
+    char **items;
+    size_t count;
+    size_t capacity;
+} str_arr_t;
+
+typedef struct {
+    char *items;
+    size_t count;
+    size_t capacity;
+} str_t;
+
+#define pressed(id) IsMouseButtonPressed(0) && Clay_PointerOver(Clay_GetElementId(CLAY_STRING(id)))
+#define released(id) IsMouseButtonReleased(0) && Clay_PointerOver(Clay_GetElementId(CLAY_STRING(id)))
+
+void HandleClayErrors(Clay_ErrorData errorData) {
+    printf("[CLAY] [ERROR] %s\n", errorData.errorText.chars);
+}
+
+int runMagick(ClayVideoDemo_Data *data) {
+    int allow_multiple_selects = true;
+    char const *filter_params[] = {"*.png", "*.svg", "*.jpg", "*.jpeg"};
+    char *input_path = tinyfd_openFileDialog("Path to image", "./", 4, filter_params, "Image file", allow_multiple_selects);
+    char *output_path = "res.png";
+    char *ashlar_path = malloc(strlen(output_path) + strlen("ashlar:") + 1);
+    *ashlar_path = '\0';
+    strcat(ashlar_path, "ashlar:");
+    strcat(ashlar_path, output_path);
+
+    if (!input_path) {
+        fprintf(stderr, "Too bad no files\n");
+        free(ashlar_path);
+        return false;
+    }
+    char c;
+    Nob_Cmd cmd = {0};
+    Nob_Cmd to_free = {0};
+    Nob_String_Builder buf = {0};
+    nob_cmd_append(&cmd, "magick", "-version");
+
+    if (!nob_cmd_run_sync_and_reset(&cmd)) {
+        fprintf(stderr, "Bro, where is yr magick??\n");
+        free(ashlar_path);
+        nob_cmd_free(cmd);
+        return false;
+    }
+    nob_cmd_append(&cmd, "magick");
+    nob_cmd_append(&cmd, "-verbose");
+
+    for (int i = 0; ; i++) {
+        c = input_path[i];
+        if (c == MULTI_SEPARATOR || c == '\0') {
+            nob_sb_append_null(&buf);
+            // moving buffer to another place on the heap
+            char *pointer = strcpy(malloc(sizeof(buf.items) * buf.count), buf.items);
+            nob_cmd_append(&cmd, pointer);
+            nob_cmd_append(&to_free, pointer);
+            buf.count = 0;
+        } else {
+            nob_sb_append_buf(&buf, &c, 1);
+        }
+        if (c == '\0') break;
+    }
+
+    char *resize_str = malloc(2 * strlen(data->resize_str) + 3); // x, \0 and modifier maybe
+    *resize_str = '\0';
+    strcat(resize_str, data->resize_str);
+    strcat(resize_str, "x");
+    strcat(resize_str, data->resize_str);
+    // strcat(resize_str, "x"); // modifier
+    nob_cmd_append(&cmd, "-resize", resize_str);
+    if (data->state & MAGICK_TRANSPARENT_BG)
+        nob_cmd_append(&cmd, "-background", "transparent");
+    if (data->state & MAGICK_BEST_FIT)
+        nob_cmd_append(&cmd, "-define", "ashlar:best-fit=true");
+    nob_cmd_append(&cmd, "-gravity", "center");
+    nob_cmd_append(&cmd, ashlar_path);
+    fprintf(stderr, "First : %s\n", cmd.items[0]);
+    fprintf(stderr, "Second: %s\n", cmd.items[1]);
+
+    bool ret = true;
+    if (nob_cmd_run_sync_and_reset(&cmd)) {
+        data->resultFile = output_path;
+        if (data->state & MAGICK_OPEN_ON_DONE) {
+            // TODO: pls add dynamic result image
+            nob_cmd_append(&cmd, "xdg-open", output_path);
+            nob_cmd_run_async(cmd);
+        }
+    } else {
+        fprintf(stderr, "Too bad, no result\n");
+        ret = false;
+    }
+    // Freeing memory
+    free(ashlar_path);
+    free(resize_str);
+    for (int i = 0; i < to_free.count; i++) {
+        // free(&to_free.items[i]);
+        printf("char: %c, pointer: %p\n", *to_free.items[i], to_free.items[i]);
+        printf("string: %s\n", to_free.items[i]);
+        free((void *) to_free.items[i]);
+    }
+    nob_cmd_free(cmd);
+    nob_cmd_free(to_free);
+    return ret;
+}
+
+// https://gitlab.com/camconn/fontconfig-example
+int defaultFont(char** res) {
+  FcConfig*       conf;
+  FcFontSet*      fs;
+  FcObjectSet*    os = 0;
+  FcPattern*      pat;
+  FcResult        result;
+
+  conf = FcInitLoadConfigAndFonts();
+  pat = FcNameParse((FcChar8*) "");
+
+  if (!pat) {
+    fprintf(stderr, "Pattern is empty\n");
+
+    return -1;
+  }
+
+  FcConfigSubstitute(conf, pat, FcMatchPattern);
+  FcDefaultSubstitute(pat);
+
+  fs = FcFontSetCreate();
+  os = FcObjectSetBuild(FC_FILE, (char*)0);
+
+  FcFontSet *font_patterns;
+  font_patterns = FcFontSort(conf, pat, FcTrue, 0, &result);
+
+  if (!font_patterns || font_patterns->nfont == 0) {
+    fprintf(stderr, "Fontconfig could not find ANY fonts on the system?\n");
+    return -1;
+  }
+
+  FcPattern *font_pattern;
+  font_pattern = FcFontRenderPrepare(conf, pat, font_patterns->fonts[0]);
+  if (font_pattern){
+    FcFontSetAdd(fs, font_pattern);
+  } else {
+    fprintf(stderr, "Could not prepare matched font for loading.\n");
+    return -1;
+  }
+
+  FcFontSetSortDestroy(font_patterns);
+  FcPatternDestroy(pat);
+
+  if (fs) {
+    if (fs->nfont > 0) {
+      FcValue v;
+      FcPattern *font;
+
+      font = FcPatternFilter(fs->fonts[0], os);
+
+      FcPatternGet(font, FC_FILE, 0, &v);
+      char* filepath = (char*)v.u.f;
+      *res = strdup(filepath);
+      FcPatternDestroy(font);
+    }
+    FcFontSetDestroy(fs);
+  } else {
+    fprintf(stderr, "could not obtain fs\n");
+  }
+
+  if (os)
+    FcObjectSetDestroy(os);
+
+  return 0;
+}
+
+Clay_RenderCommandArray CreateLayout(Clay_Context* context, ClayVideoDemo_Data *data) {
+    Clay_SetCurrentContext(context);
+#ifdef TESTING
+    // Clay_SetDebugModeEnabled(true);
+#endif
+    // Run once per frame
+    Clay_SetLayoutDimensions((Clay_Dimensions) {
+            .width = GetScreenWidth(),
+            .height = GetScreenHeight()
+    });
+    Vector2 mousePosition = GetMousePosition();
+    Vector2 scrollDelta = GetMouseWheelMoveV();
+    Clay_SetPointerState(
+            (Clay_Vector2) { mousePosition.x, mousePosition.y },
+            IsMouseButtonDown(0)
+    );
+    Clay_UpdateScrollContainers(
+            true,
+            (Clay_Vector2) { scrollDelta.x, scrollDelta.y },
+            GetFrameTime()
+    );
+
+    sprintf(data->resize_str, "%d", data->resize);
+    if (released("Quit")) {
+        data->shouldClose = true;
+        printf("close\n");
+    } else if (pressed("Select_Images")) {
+    } else if (released("Select_Images")) {
+        runMagick(data);
+    } else if (released("Open result")) {
+        if (data->resultFile[0] != '\0') {
+            Nob_Cmd cmd = {0};
+            nob_cmd_append(&cmd, "xdg-open", data->resultFile);
+            nob_cmd_run_async(cmd);
+            nob_cmd_free(cmd);
+        } else {
+            fprintf(stderr, "No file was made\n");
+        }
+    } else if (Clay_PointerOver(Clay_GetElementId(CLAY_STRING("Resize")))) {
+        if (IsMouseButtonPressed(0) || scrollDelta.y > 0 || IsKeyPressed(KEY_RIGHT) || IsKeyPressed(KEY_UP))
+            data->resize += 50;
+        else if (IsMouseButtonPressed(1) || scrollDelta.y < 0 || IsKeyPressed(KEY_LEFT) || IsKeyPressed(KEY_DOWN))
+            data->resize -= 50;
+
+        // fprintf(stderr, "pressed resize: %d\n", data->resize);
+    }
+
+    return ClayVideoDemo_CreateLayout(data);
+}
+
+int main(void) {
+    char* title = "Magick deez nuts";
+    // vsync makes resizes slower, we don't want this
+    // but antialiasing is nice
+    Clay_Raylib_Initialize(1024, 768, title, FLAG_WINDOW_RESIZABLE | FLAG_MSAA_4X_HINT);
+
+    Font fonts[1];
+    char *fontpath = {0};
+    if (defaultFont(&fontpath) == -1) {
+        return -1;
+    }
+    // https://stackoverflow.com/questions/73248125/raylibs-drawtextex-doesnt-display-unicode-characters
+    int codepoints[512] = {0};
+    for (int i = 0; i < 95; i++) codepoints[i] = 32 + i;
+    for (int i = 0; i < 255; i++) codepoints[96 + i] = 0x400 + i;
+    fonts[FONT_ID_BODY_16] = LoadFontEx(fontpath, FONT_LOAD_SIZE, codepoints, 512);
+    free(fontpath);
+    SetTextureFilter(fonts[FONT_ID_BODY_16].texture, TEXTURE_FILTER_BILINEAR);
+    uint64_t clayRequiredMemory = Clay_MinMemorySize();
+
+    Clay_Arena clayMemory = Clay_CreateArenaWithCapacityAndMemory(clayRequiredMemory, malloc(clayRequiredMemory));
+    Clay_Context *clayContext = Clay_Initialize(clayMemory, (Clay_Dimensions) {
+       .width = GetScreenWidth(),
+       .height = GetScreenHeight()
+    }, (Clay_ErrorHandler) { HandleClayErrors }); // This final argument is new since the video was published
+    ClayVideoDemo_Data data = ClayVideoDemo_Initialize();
+    Clay_SetMeasureTextFunction(Raylib_MeasureText, fonts);
+
+    while (!WindowShouldClose()) {
+        Clay_RenderCommandArray renderCommandsTop = CreateLayout(clayContext, &data);
+        if (data.shouldClose) break;
+
+        BeginDrawing();
+        ClearBackground(BLACK);
+        Clay_Raylib_Render(renderCommandsTop, fonts);
+#ifdef TESTING
+        DrawFPS(0,0);
+#endif // TESTING
+        EndDrawing();
+    }
+
+    Clay_Raylib_Close();
+}
