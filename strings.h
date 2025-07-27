d510385da848a34ba63e22e6740f0d27c0cbca2f
Yerlotic <Yerlotic@users.noreply.github.com>
2025-07-27 15:38:22 +0300

    Add option to set tempdir location

---
+++ strings.h
@@ -0,0 +1,3 @@
+#define SELECT_TEMP "Change location"
+#define TEMP_FILES "Temporary files"
+#define TEMP_FILES_EXPLANATION "When changed, it uses available storage as memory. This option allows to work around memory issues but makes everything slower"
