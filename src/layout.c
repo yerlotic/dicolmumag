63bc9c6150bfcf0615eceb8e2b4d4a68220a65a3
Yerlotic <Yerlotic@users.noreply.github.com>
2025-10-19 15:48:11 +0300

    Move some code to ui.c, rename structs

--- src/layout.c
+++ src/layout.c
@@ -1,47 +1,14 @@
-#include "thirdparty/clay.h"
 #include <stdint.h>
 #include <stdlib.h>
-#include <string.h>
 
-// #define LATTE
-#include "colors.c"
 #include "thirdparty/cthreads.h"
 #include "thirdparty/raylib/raylib.h"
 #include "thirdparty/tinyfiledialogs.h"
+#include "thirdparty/clay.h"
 #define NOB_IMPLEMENTATION
 #include "thirdparty/nob.h"
 #include "strings.c"
-
-#ifdef NO_SCALING
-const float scale = 1.0;
-#define S(x) (x)
-#else
-float scale = 1.0;
-#define S(x) ((int) (x) * scale)
-#endif // NO_SCALING
-
-const uint8_t FONT_ID_BODY_16 = 0;
-const uint8_t FONT_ID_SIDEBAR = 1;
-const uint8_t FONT_ID_BUTTONS = 2;
-const uint8_t FONT_ID_DOCUMNT = 3;
-const uint8_t FONT_ID_WELCOME = 4;
-const uint8_t FONTS_IDS = 5;
-const uint8_t FONT_LOAD_SIZE = 40;
-const uint8_t button_font_size = 20;
-const uint8_t sidebar_font_size = 23;
-const uint8_t document_font_size = 30;
-const uint8_t title_font_size = 40;
-const uint8_t welcome_font_size = 80;
-
-#define CLAY_DYNAMIC_STRING(string) (CLAY__INIT(Clay_String) { .isStaticallyAllocated = false, .length = strlen(string), .chars = (string) })
-#define CLAY_SB_STRING(sb) (CLAY__INIT(Clay_String) { .isStaticallyAllocated = false, .length = (sb).count, .chars = (sb).items })
-#define BUTTON_RADIUS CLAY_CORNER_RADIUS(S(10))
-#define LAYOUT_RADIUS CLAY_CORNER_RADIUS(S(10))
-#define JUST_TEXT_CONFIG(font_size, font_id, color) CLAY_TEXT_CONFIG({ .fontId = (font_id), .fontSize = S(font_size), .textColor = (color) })
-#define SANE_TEXT_CONFIG(font_size, font_id) JUST_TEXT_CONFIG((font_size), (font_id), colors[colorscheme][COLOR_TEXT])
-#define DEFAULT_TEXT  SANE_TEXT_CONFIG(document_font_size, FONT_ID_DOCUMNT)
-#define DISABLED_TEXT JUST_TEXT_CONFIG(document_font_size, FONT_ID_DOCUMNT, colors[colorscheme][COLOR_OVERLAY0])
-#define BUTTON_TEXT   SANE_TEXT_CONFIG(button_font_size,   FONT_ID_BUTTONS)
+#include "ui.c"
 
 #define MAGICK_RESIZE_IGNORE_RATIO    "!"
 #define MAGICK_RESIZE_SHRINK_LARGER   ">"
@@ -49,101 +16,6 @@ const uint8_t welcome_font_size = 80;
 #define MAGICK_RESIZE_FILL_AREA       "^"
 #define MAGICK_RESIZE_PERCENTAGE      "%"
 #define MAGICK_RESIZE_PIXEL_LIMIT     "@"
-
-static inline void RenderHeaderButton(Clay_String text) {
-    CLAY({
-        .layout = {
-            .padding = { S(16), S(16), S(8), S(8) },
-            .childAlignment = {
-                .x = CLAY_ALIGN_X_CENTER,
-                .y = CLAY_ALIGN_Y_CENTER,
-            },
-        },
-        .backgroundColor = BUTTON_COLOR,
-        .id = CLAY_SID(text),
-        .cornerRadius = BUTTON_RADIUS,
-    }) {
-        CLAY_TEXT(text, CLAY_TEXT_CONFIG({
-            .fontId = FONT_ID_BUTTONS,
-            .fontSize = S(button_font_size),
-            .textColor = colors[colorscheme][COLOR_TEXT],
-            .textAlignment = CLAY_TEXT_ALIGN_CENTER,
-            .wrapMode = CLAY_TEXT_WRAP_NONE,
-        }));
-    }
-}
-
-static inline void RenderDropdownMenuItem(Clay_String text) {
-    CLAY({
-        .backgroundColor = colors[colorscheme][COLOR_SURFACE0],
-        .cornerRadius = BUTTON_RADIUS,
-        .id = CLAY_SID(text),
-        .layout = {
-            .padding = CLAY_PADDING_ALL(S(16)),
-            .sizing = {
-                .width = CLAY_SIZING_GROW(0)
-            },
-    }}) {
-        CLAY_TEXT(text, BUTTON_TEXT);
-    }
-}
-
-static inline void RenderNumberPicker(char* value) {
-    CLAY({
-        .layout = { .padding = { S(16), S(16), S(8), S(8) }},
-        .backgroundColor = BUTTON_COLOR,
-        // .id = CLAY_SID(name),
-        .cornerRadius = BUTTON_RADIUS,
-    }) {
-        CLAY_TEXT(CLAY_DYNAMIC_STRING(value), BUTTON_TEXT);
-    }
-}
-
-static inline void RenderColorChannel(Clay_String text, Clay_Color color, char *value) {
-    CLAY({
-        .layout = {
-            .layoutDirection = CLAY_LEFT_TO_RIGHT,
-        },
-        .id = CLAY_SID(text),
-    }) {
-        CLAY({ .layout = { .sizing = { .width = S(25) }}}) {
-            CLAY_TEXT(text, CLAY_TEXT_CONFIG({
-                .fontId = FONT_ID_BODY_16,
-                .fontSize = S(document_font_size),
-                .textColor = color,
-            }));
-            CLAY_TEXT(CLAY_STRING(":"), DEFAULT_TEXT);
-        }
-        RenderNumberPicker(value);
-    }
-}
-
-static inline void RenderColor(Clay_Color color) {
-    uint8_t border_width = 5;
-    uint8_t radius = 5;
-    uint8_t size = 50;
-    CLAY({
-        .cornerRadius = CLAY_CORNER_RADIUS(S(radius)),
-        .backgroundColor = color,
-        .border = {
-            .color = TERNARY_COLOR(Clay_Hovered(), colors[colorscheme][COLOR_SURFACE1], colors[colorscheme][COLOR_SURFACE0]),
-            .width = CLAY_BORDER_OUTSIDE(S(border_width))
-        },
-        .layout = {
-            .sizing = {
-                .height = CLAY_SIZING_FIXED(S(size)),
-                .width = CLAY_SIZING_FIXED(S(size))
-            },
-        },
-    }) {}
-}
-
-static inline void StatedText(Clay_String text, bool enabled) {
-    if (enabled)
-        CLAY_TEXT(text, DEFAULT_TEXT);
-    else
-        CLAY_TEXT(text, DISABLED_TEXT);
-}
 
 typedef struct {
     Clay_String title;
@@ -167,7 +39,7 @@ DocumentArray documents = {
 typedef struct {
     intptr_t offset;
     intptr_t memory;
-} ClayVideoDemo_Arena;
+} AppArena;
 
 typedef enum {
     MAGICK_BEST_FIT        = 1 << 0,
@@ -264,14 +136,14 @@ typedef struct {
     uint8_t selectedDocumentIndex;
     MagickStatus errorIndex;
     float yOffset;
-    ClayVideoDemo_Arena frameArena;
+    AppArena frameArena;
     magick_params_t params;
     // These should be stored on the heap to prevent
     // race condition caused by stack being gone
     struct cthreads_thread magickThread;
     struct cthreads_args threadArgs;
     bool shouldClose;
-} ClayVideoDemo_Data;
+} AppData;
 
 typedef struct {
     uint16_t flag;
@@ -378,13 +250,13 @@ static inline void RenderFlag(Clay_String text,
                 uint16_t *state,
                 uint16_t triggerFlag,
                 uint16_t displayFlag,
-                ClayVideoDemo_Arena *arena) {
+                AppArena *arena) {
     Clay_Color background = BUTTON_COLOR;
     if (*state & displayFlag)
         background = OPACITY(colors[colorscheme][COLOR_GREEN], 35);
     CLAY({
         .layout = {
-            .padding = { S(16), S(16), S(8), S(8) },
+            .padding = defaultPadding,
             .childAlignment = {
                 .x = CLAY_ALIGN_X_CENTER,
                 .y = CLAY_ALIGN_Y_CENTER,
@@ -409,7 +281,7 @@ static inline void RenderFlag(Clay_String text,
     }
 }
 
-ClayVideoDemo_Data ClayVideoDemo_Initialize() {
+AppData AppDataInit() {
     // Update DocumentArray
     documents.documents[MAGICK_BEST_FIT_I] = (Document){ .title = CLAY_STRING(TEXT_BEST_FIT), .contents = CLAY_STRING(TEXT_BEST_FIT_EXPL) };
     documents.documents[MAGICK_TRANSPARENT_BG_I] = (Document){ .title = CLAY_STRING(TEXT_TRANSPARENT_BG), .contents = CLAY_STRING(TEXT_TRANSPARENT_BG_EXPL) };
@@ -419,7 +291,7 @@ ClayVideoDemo_Data ClayVideoDemo_Initialize() {
     documents.documents[MAGICK_ADVANCED_SETTINGS] = (Document){ .title = CLAY_STRING(ADVANCED_SETTINGS_S), .contents = CLAY_STRING(TEXT_ADVANCED_SETTINGS_EXPL) };
     documents.documents[MAGICK_WELCOME_PAGE_I] = (Document){0};
 
-    ClayVideoDemo_Data data = {
+    AppData data = {
         .frameArena = { .memory = (intptr_t)malloc(1024) },
         .shouldClose = false,
         // .selectedDocumentIndex = MAGICK_ADVANCED_SETTINGS,
@@ -503,7 +375,7 @@ ClayVideoDemo_Data ClayVideoDemo_Initialize() {
     return data;
 }
 
-Clay_RenderCommandArray ClayVideoDemo_CreateLayout(ClayVideoDemo_Data *data) {
+Clay_RenderCommandArray AppCreateLayout(AppData *data) {
     data->frameArena.offset = 0;
 
     Clay_BeginLayout();
@@ -514,7 +386,7 @@ Clay_RenderCommandArray ClayVideoDemo_CreateLayout(ClayVideoDemo_Data *data) {
     };
 
     Clay_Color contentBackgroundColor = colors[colorscheme][COLOR_BASE];
-
+    defaultPadding = (Clay_Padding) { S(16), S(16), S(8), S(8) };
     // Build UI here
     CLAY({
         .id = CLAY_ID(ID_OUTER),
@@ -546,7 +418,7 @@ Clay_RenderCommandArray ClayVideoDemo_CreateLayout(ClayVideoDemo_Data *data) {
             // Header buttons go here
             CLAY({
                 .id = CLAY_ID(ID_FILE_BUTTON),
-                .layout = { .padding = { S(16), S(16), S(8), S(8) }},
+                .layout = { .padding = defaultPadding},
                 .backgroundColor = BUTTON_COLOR,
                 .cornerRadius = BUTTON_RADIUS,
             }) {
@@ -618,7 +490,7 @@ Clay_RenderCommandArray ClayVideoDemo_CreateLayout(ClayVideoDemo_Data *data) {
             }
             CLAY({ .layout = { .sizing = { CLAY_SIZING_GROW(0) }}}) {}
             CLAY({
-                .layout = { .padding = { S(16), S(16), S(8), S(8) }},
+                .layout = { .padding = defaultPadding},
                 .backgroundColor = BUTTON_COLOR,
                 .id = CLAY_ID(ID_RESIZE_ALL),
                 .cornerRadius = BUTTON_RADIUS,
@@ -763,7 +635,7 @@ Clay_RenderCommandArray ClayVideoDemo_CreateLayout(ClayVideoDemo_Data *data) {
                                     .id = CLAY_ID(ID_GRAVITY),
                                     .cornerRadius = BUTTON_RADIUS,
                                     }) {
-                                CLAY({ .id = CLAY_ID(ID_GRAVITY_SELECTION), .layout = { .padding = { S(16), S(16), S(8), S(8) } } }) {
+                                CLAY({ .id = CLAY_ID(ID_GRAVITY_SELECTION), .layout = { .padding = defaultPadding } }) {
                                     CLAY_TEXT(CLAY_DYNAMIC_STRING(data->params.gravity.values[data->params.gravity.selected]), BUTTON_TEXT);
                                 }
                             }
@@ -878,7 +750,7 @@ Clay_RenderCommandArray ClayVideoDemo_CreateLayout(ClayVideoDemo_Data *data) {
                         Clay_String start = CLAY_STRING(START_USING);
                         CLAY({
                             .layout = {
-                                .padding = { S(16), S(16), S(8), S(8) },
+                                .padding = defaultPadding,
                                 .childAlignment = {
                                     .x = CLAY_ALIGN_X_CENTER,
                                     .y = CLAY_ALIGN_Y_CENTER,
